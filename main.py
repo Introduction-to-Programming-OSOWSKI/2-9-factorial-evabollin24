d0c6e2cc895f0ce87b63efdf3c8a25dbf3e35a1b
evabollin24 <817972@mystma.org>
2021-10-06 08:57:11 -0500

    2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def factorial(x):
-    factorial = 1
+    total = 1
     for i in range(1, x + 1):
-        factorial = factorial * i
-    return(factorial)
-    
+        total = total * i
+    return(total)
